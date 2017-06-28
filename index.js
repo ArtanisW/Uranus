be9389042c7e4af08a2f52b5e2e319abe89a10d1
ArtanisW <wjqwonder@hotmail.com>
2017-06-28 14:02:41 +0800

    init project
    Build the basic structure of the project

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+const cheerio = require('cheerio');
+const request = require('request');
+let i = 0;
+const URL = '';
+
+function fetchPage(url) {
+    startRequest(url)
+}
+
+function startRequest(url) {
+
+}
+
+fetchPage(URL);
